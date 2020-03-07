c770e63919dc286aae88dd21965494aa3401dd64
Jan Häusler <jan.haeusler@fir.rwth-aachen.de>
2020-03-07 18:47:32 +0100

    Implement first iteration of "authentication".

--- src/example.ts
+++ src/example.ts
@@ -1,6 +1,9 @@
 import { Platform } from '.';
 
-const platform = new Platform(process.env.PLATFORM_HOST ? process.env.PLATFORM_HOST : '');
+const platform = new Platform(
+  process.env.PLATFORM_HOST ? process.env.PLATFORM_HOST : '',
+  process.env.THING_ID ? process.env.THING_ID : '',
+);
 // tslint:disable-next-line:no-console
 platform.on('connect', () => console.log('connected'));
 // tslint:disable-next-line:no-console
--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,24 @@ import { EventEmitter } from 'events';
 
 class Platform extends EventEmitter {
   socket: SocketIOClient.Socket;
+  thingId: string;
 
-  constructor(host: string) {
+  constructor(host: string, thingId: string) {
     super();
     this.socket = io(host);
+    this.thingId = thingId;
+
+    this.setupEventForwarding();
+    this.socket.on('connect', () => {
+      this.socket.emit('auth', { id: this.thingId });
+    });
   }
 
   connect() {
     this.socket.connect();
-
-    this.registerWebsocketEvents();
   }
 
-  private registerWebsocketEvents() {
+  private setupEventForwarding() {
     const events = ['connect', 'disconnect', 'event', 'connect_error', 'connect_timeout', 'methodCall'];
 
     events.forEach(event => {
