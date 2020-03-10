397e26eeb5942ddc5fecbba52ad63dd80e0bd71c
Jan Häusler <jan.haeusler@fir.rwth-aachen.de>
2020-03-10 18:06:45 +0100

    Update example to allow console.log.

--- src/example.ts
+++ src/example.ts
@@ -4,8 +4,9 @@ const platform = new Platform(
   process.env.PLATFORM_HOST ? process.env.PLATFORM_HOST : '',
   process.env.THING_ID ? process.env.THING_ID : '',
 );
-// tslint:disable-next-line:no-console
+
 platform.on('authenticated', () => {
+  // tslint:disable-next-line:no-console
   console.log('authenticated');
   platform.setAttributeValue('occupancyState', 2);
 });
