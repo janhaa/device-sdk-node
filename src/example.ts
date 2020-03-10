2ce89d1014c72e144cdf538d10679907c0d8040e
Jan Häusler <jan.haeusler@fir.rwth-aachen.de>
2020-03-10 17:58:37 +0100

    Update example.

--- src/example.ts
+++ src/example.ts
@@ -5,7 +5,10 @@ const platform = new Platform(
   process.env.THING_ID ? process.env.THING_ID : '',
 );
 // tslint:disable-next-line:no-console
-platform.on('connect', () => console.log('connected'));
+platform.on('authenticated', () => {
+  console.log('authenticated');
+  platform.setAttributeValue('occupancyState', 2);
+});
 // tslint:disable-next-line:no-console
 platform.on('method-invocation', call => console.log(call));
 
